d16a19a50e4225b4fb561148a507873deefc86b1
julia <julia.anjos.017@ufrn.edu.br>
2021-11-16 20:43:05 -0300

    bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div>
-      <h1>Olá, Mundo!</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="text-primary">Olá, Mundo!</h1>
+        </div>
+    );
 }
 
 export default App;
